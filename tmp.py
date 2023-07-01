fa29a3363a1fd75594bd0d0d57f56930d613739a
honoka <hello@hello_world.com>
2023-07-01 17:05:13 +0900

    [refactor] remove files

--- tmp.py
+++
@@ -1,7 +0,0 @@
-API_KEY = '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'
-
-from langchain.llms import OpenAI
-llm = OpenAI(openai_api_key=API_KEY)
-
-text = "What would be a good company name for a company that makes colorful socks?"
-print(llm(text))
