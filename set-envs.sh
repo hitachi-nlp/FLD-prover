e436411c77a0ae292548dbbf64a50131f37628d5
honoka <hello@hello_world.com>
2023-07-02 12:55:42 +0900

    [refactor] remove files

--- set-envs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=`pwd -P`:`readlink -f ./launchers`:$PROJECTS/FLD-task/:$PROJECTS/kern-profiler/:$PROJECTS/script-engine/:$PROJECTS/lab:$PROJECTS:$PROJECTS/python-logger-setup::${PYTHONPATH}
