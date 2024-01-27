b0564eba689af38a6c75596d3284c2df99b6ebb8
MorishT <magi.magilmagi@gmail.com>
2024-01-27 11:40:11 +0900

    [exp] ./outputs/01.train.py/20230120.jpn.punipuni

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -178,25 +178,25 @@ def main():
 
         # ---------------------------------- 20230120.jpn.punipuni ------------------------------------
 
-        '20230120.jpn.wordnet_repro_w_proposition.D1_wo_dist',
-        '20230120.jpn.wordnet_repro_w_proposition.D1',
-        '20230120.jpn.wordnet_repro_w_proposition.D3',
-        '20230120.jpn.wordnet_repro_w_proposition.D8',
+        # '20230120.jpn.wordnet_repro_w_proposition.D1_wo_dist',
+        # '20230120.jpn.wordnet_repro_w_proposition.D1',
+        # '20230120.jpn.wordnet_repro_w_proposition.D3',
+        # '20230120.jpn.wordnet_repro_w_proposition.D8',
 
-        '20230120.jpn.wordnet_repro_wo_proposition.D1_wo_dist',
-        '20230120.jpn.wordnet_repro_wo_proposition.D1',
-        '20230120.jpn.wordnet_repro_wo_proposition.D3',
-        '20230120.jpn.wordnet_repro_wo_proposition.D8',
+        # '20230120.jpn.wordnet_repro_wo_proposition.D1_wo_dist',
+        # '20230120.jpn.wordnet_repro_wo_proposition.D1',
+        # '20230120.jpn.wordnet_repro_wo_proposition.D3',
+        # '20230120.jpn.wordnet_repro_wo_proposition.D8',
 
-        '20230120.jpn.BCCWJ.D1_wo_dist',
-        '20230120.jpn.BCCWJ.D1',
-        '20230120.jpn.BCCWJ.D3',
+        # '20230120.jpn.BCCWJ.D1_wo_dist',
+        # '20230120.jpn.BCCWJ.D1',
+        # '20230120.jpn.BCCWJ.D3',
         '20230120.jpn.BCCWJ.D8',
 
-        '20230120.jpn.punipuni.D1_wo_dist',
-        '20230120.jpn.punipuni.D1',
-        '20230120.jpn.punipuni.D3',
-        '20230120.jpn.punipuni.D8',
+        # '20230120.jpn.punipuni.D1_wo_dist',
+        # '20230120.jpn.punipuni.D1',
+        # '20230120.jpn.punipuni.D3',
+        # '20230120.jpn.punipuni.D8',
     ]
 
     # other_dataset_name = "wikitext"
@@ -378,8 +378,8 @@ def main():
 
     # engine = SubprocessEngine()
     # engine = QsubEngine('ABCI', 'rt_G.large', n_resource=1)
-    engine = QsubEngine('ABCI', 'rt_G.large', n_resource=2)
-    # engine = QsubEngine('ABCI', 'rt_F', n_resource=4)  # 10b model for sppedup. Note that n_resouce=3 does not yield batch size 32
+    # engine = QsubEngine('ABCI', 'rt_F', n_resource=2)
+    engine = QsubEngine('ABCI', 'rt_F', n_resource=4)  # 10b model for sppedup. Note that n_resouce=3 does not yield batch size 32
     # engine = QsubEngine('ABCI', 'rt_F', n_resource=16)   # 70b model
 
     if isinstance(engine, SubprocessEngine):
