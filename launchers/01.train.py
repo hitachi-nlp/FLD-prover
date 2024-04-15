d78392a7abec8648110a1b3579d15bd74a9c4f1d
MorishT <magi.magilmagi@gmail.com>
2024-04-15 13:35:31 +0900

    [update] ./outputs/01.train.py/2024-4-15.FLD.v2.centered with old FLD.v2

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -246,9 +246,9 @@ def main():
 
         # ---------------------------------- other datasets ------------------------------------
 
-        # 'hf.hitachi-nlp/FLD.v2__default',
+        'hf.hitachi-nlp/FLD.v2__default',
 
-        '2024-03-29.FLD_v2',
+        # '2024-03-29.FLD_v2',
         # 'hf.hitachi-nlp/ruletaker',
         # 'hf.hitachi-nlp/PARARULE-Plus',
         # 'hf.hitachi-nlp/proofwriter_processed_OWA__depth-3ext',
@@ -256,6 +256,8 @@ def main():
         # # 'hf.tasksource/robustLR',  # the training dataset is small, might be "test-only" dataset.
     ]
 
+    # logic_dataset_concatenate_all_configs = True
+    # logic_dataset_concatenate_all_splits_into_train = True
     logic_dataset_concatenate_all_configs = False
     logic_dataset_concatenate_all_splits_into_train = False
 
@@ -407,11 +409,11 @@ def main():
         # 'FT.bs-256__step-152',      # NeurIPS Alpaca 3 epochs with context 2048
         # 'FT.bs-256__step-400',        # NeurIPS 100k slim-pajama
         # 'FT.bs-256__step-520',          # NeurIPS 100k, logic=0.75
-        # 'FT.bs-256__step-800',        # NeurIPS 100k, logic=0.5
+        'FT.bs-256__step-800',        # NeurIPS 100k, logic=0.5
         # 'FT.bs-256__step-1600'          # NeurIPS 100k, logic=0.25
 
         # 'FT.bs-512__step-1200',
-        'FT.bs-768__step-1200',
+        # 'FT.bs-768__step-1200',
 
         # 'FT.bs-256__step-1250',   # JSAI
         # 'FT.bs-256__step-2500',
@@ -484,9 +486,9 @@ def main():
     # engine = QsubEngine('ABCI', 'rt_F', n_resource=32)   # 70B model
 
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=1)
-    # engine = QsubEngine('haic', 'xhn_s.large', n_resource=2)
+    engine = QsubEngine('haic', 'xhn_s.large', n_resource=2)
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=3)
-    engine = QsubEngine('haic', 'xhn_s.large', n_resource=6)
+    # engine = QsubEngine('haic', 'xhn_s.large', n_resource=6)
 
     hours = 24
 
