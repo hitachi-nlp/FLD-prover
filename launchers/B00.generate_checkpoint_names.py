a1a441fa74cfbf635249a7e2e12b14922b04223f
MorishT <magi.magilmagi@gmail.com>
2024-04-21 18:31:46 +0900

    [add] B00.generate_checkpoint_names.py

---
+++ launchers/B00.generate_checkpoint_names.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import logging
+from pathlib import Path
+from typing import List, Optional, Tuple, Dict
+import json
+from collections import OrderedDict
+
+from logger_setup import setup as setup_logger
+import click
+from lab import make_name
+
+
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+def main():
+    setup_logger()
+
+    TOP_DIR = './outputs.FLD-prover/01.train.py/2024-4-20.production.additional/'
+
+    CHECKPOINTS = [
+        97,
+        194,
+        291,
+        388,
+    ]
+
+    PARAMS = [
+        'logic_dataset_uname',
+        'learning',
+        'learning_rate',
+        'lr_scheduler_type',
+    ]
+
+    input_dir = Path(TOP_DIR)
+    setting_paths = sorted(input_dir.glob('**/*/lab.params.json'))
+    for setting_path in setting_paths:
+        for checkpoint in CHECKPOINTS:
+            settings = json.load(open(str(setting_path)))
+            _settings = OrderedDict([
+                (key, settings[key])
+                for key in PARAMS
+            ])
+            name = make_name(_settings, sep='__', short=True)
+            print(f"'{name}.chk-{checkpoint}': '" + str(setting_path.parent / f"checkpoint-{checkpoint}',"))
+
+
+if __name__ == '__main__':
+    main()
