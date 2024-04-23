7448aae8d9fa70dfd116fef482772781ff73c767
MorishT <magi.magilmagi@gmail.com>
2024-04-24 01:13:42 +0900

    [exp] ./outputs/01.train.py/2024-4-23.refine_production.more

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -310,13 +310,13 @@ def main():
         # 'hf.hitachi-nlp/FLD.v2__default',
 
 
-        '2024-03-29.FLD_v2',
+        # '2024-03-29.FLD_v2',
         # '2024-03-29.FLD_v2.D8',
         # '2024-03-29.FLD_v2.theorems-0.03',
 
-        # '2024-03-29.FLD_v2.theorems-0.3.fix',
-        # '2024-03-29.FLD_v2.theorems-0.1.fix',
-        # '2024-03-29.FLD_v2.theorems-0.03.fix',
+        '2024-03-29.FLD_v2.theorems-0.3.fix',
+        '2024-03-29.FLD_v2.theorems-0.1.fix',
+        '2024-03-29.FLD_v2.theorems-0.03.fix',
 
 
         # '2024-03-29.JSAI_best.no_aug',
@@ -412,13 +412,13 @@ def main():
 
 
         # --------- dataset = 100k, logic=1.0--------------------
-        'FT.bs-256__step-390.wrmp-200',
+        # 'FT.bs-256__step-390.wrmp-200',
 
         # --------- dataset = 200k, logic=1.0--------------------
         'FT.bs-256__step-780.wrmp-400',
 
         # --------- dataset = 300k, logic=1.0--------------------
-        'FT.bs-256__step-1172.wrmp-600',
+        # 'FT.bs-256__step-1172.wrmp-600',
 
 
         # --------- dataset = 300k, logic=1.0 -----------
@@ -479,12 +479,12 @@ def main():
 
         # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 300),
         ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 3000),
-        ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 10000),
+        # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 10000),
         # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 30000),
 
         # ('rec_adam', 'l2', 0.5, 'gradually_from_middle', 300),
         ('rec_adam', 'l2', 0.5, 'gradually_from_middle', 3000),
-        ('rec_adam', 'l2', 0.5, 'gradually_from_middle', 10000),
+        # ('rec_adam', 'l2', 0.5, 'gradually_from_middle', 10000),
         # ('rec_adam', 'l2', 0.5, 'gradually_from_middle', 30000),
 
         # l1 regularization always ends up with mess
