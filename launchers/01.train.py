c85fdff83c762dae31d4297f89a3e8baa3c4c9cb
MorishT <magi.magilmagi@gmail.com>
2024-05-01 22:09:33 +0900

    [exp] ./outputs/00.evaluate_LLM.py/2024-4-24.finalize_production.other_llms stable lm

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -333,9 +333,9 @@ def main():
         # ---------------------------------- NeurIPS 2024 ------------------------------------
 
         'hf.hitachi-nlp/ruletaker',
-        # 'hf.hitachi-nlp/PARARULE-Plus',
-        # 'hf.hitachi-nlp/FLD.v2__default',
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing',
+        'hf.hitachi-nlp/PARARULE-Plus',
+        'hf.hitachi-nlp/FLD.v2__default',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing',
 
 
         # '2024-03-29.FLD_v2',
@@ -500,8 +500,8 @@ def main():
     # (optimizer, regularization, anneal_target_task_weight, fisher_coef)
     optimizer_setings = [
         # (None, None, None, None, None, None, None),
-        ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 0),   # should be the same as vanilla adam
-        # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 3000),   # the best
+        # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 0),   # should be the same as vanilla adam
+        ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 3000),   # the best
     ]
 
 
