b23b38e37f794caf5fead0a9bce759f2e66900f3
MorishT <magi.magilmagi@gmail.com>
2024-05-08 00:32:23 +0900

    [exp] ./outputs/01.train.py/2024-4-24.finalize_production.other_llms with mistral

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -121,8 +121,8 @@ def main():
     # output_top_dir = Path('./outputs/01.train.py/2024-4-24.finalize_production')
     # output_top_dir = Path('./outputs/01.train.py/2024-4-24.finalize_production.additional')
 
-    # output_top_dir = Path('./outputs/01.train.py/2024-4-24.finalize_production.other_llms')
-    output_top_dir = Path('./outputs/01.train.py/2024-05-03.ablation')
+    output_top_dir = Path('./outputs/01.train.py/2024-4-24.finalize_production.other_llms')
+    # output_top_dir = Path('./outputs/01.train.py/2024-05-03.ablation')
 
 
 
@@ -182,7 +182,7 @@ def main():
         # ('meta-llama/Llama-2-70b-hf', 'causal', 'meta-llama/Llama-2-70b-hf'),
 
 
-        ('meta-llama/Meta-Llama-3-8B', 'causal', 'meta-llama/Llama-2-7b-hf'),
+        # ('meta-llama/Meta-Llama-3-8B', 'causal', 'meta-llama/Llama-2-7b-hf'),
         # ('meta-llama/Meta-Llama-3-70B', 'causal', 'meta-llama/Llama-2-70b-hf'),
 
 
@@ -198,7 +198,7 @@ def main():
         # ('lmsys/vicuna-33b-v1.3', 'causal', 'meta-llama/Llama-2-70b-hf'),
 
 
-        # ('mistralai/Mistral-7B-v0.1', 'causal', 'meta-llama/Llama-2-7b-hf'),
+        ('mistralai/Mistral-7B-v0.1', 'causal', 'meta-llama/Llama-2-7b-hf'),
         # ('mistralai/Mixtral-8x7B-v0.1', 'causal', 'meta-llama/Llama-2-70b-hf'),
 
 
@@ -341,7 +341,7 @@ def main():
         # 'hf.hitachi-nlp/ruletaker',
         # 'hf.hitachi-nlp/PARARULE-Plus',
         # 'hf.hitachi-nlp/FLD.v2__default',
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing',
 
 
         # ------------------------------- 2024-05-03.ablation --------------------------------
@@ -351,9 +351,9 @@ def main():
 
         # '2024-03-29.JSAI_best.no_aug.trnsl-thing.rule-G_MP',
         # '2024-03-29.JSAI_best.no_aug.trnsl-thing.stps-3',
-        '2024-03-29.JSAI_best.no_aug.trnsl-thing.stps-5-3',
-        '2024-03-29.JSAI_best.no_aug.trnsl-thing.stps-8-0',
-        '2024-03-29.JSAI_best.no_aug.trnsl-thing.stps-1-2',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.stps-5-3',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.stps-8-0',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.stps-1-2',
 
         # '2024-03-29.JSAI_best.no_aug.trnsl-thing.rule-G_MP.stps-3',
 
@@ -531,15 +531,15 @@ def main():
     optimizer_setings = [
         # (None, None, None, None, None, None, None),
         # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 1000),
-        ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 3000),   # the best for llama3
-        # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 10000),
+        # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 3000),   # the best for llama3
+        ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 10000),
         # ('rec_adam', 'l2', 0.5, 'immediately_from_beginning', 30000),
     ]
 
 
     lrates = [
-        # 1e-5,
-        3e-6,    # the best
+        1e-5,
+        # 3e-6,    # the best
         # 1e-6,
     ]
 
