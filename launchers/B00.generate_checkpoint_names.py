09afe456eb78d4f8a689d7fa9a5960158f62edd0
MorishT <magi.magilmagi@gmail.com>
2024-05-13 23:34:59 +0900

    [exp] ./outputs/01.train.py/2024-5-13.large_models

--- launchers/B00.generate_checkpoint_names.py
+++ launchers/B00.generate_checkpoint_names.py
@@ -27,7 +27,8 @@ def main():
     # TOP_DIR = './outputs.FLD-prover/01.train.py/2024-4-24.finalize_production.other_llms.mistral_tokenizer'
     # TOP_DIR = './outputs.FLD-prover/01.train.py/2024-05-03.ablation'
     # TOP_DIR = './outputs.FLD-prover/01.train.py/2024-05-08.ref_prob'
-    TOP_DIR = './outputs.FLD-prover/01.train.py/2024-4-24.finalize_production.other_llms.tokenizer_unk'
+    # TOP_DIR = './outputs.FLD-prover/01.train.py/2024-4-24.finalize_production.other_llms.tokenizer_unk'
+    TOP_DIR = './outputs.FLD-prover//01.train.py/2024-5-13.large_models'
 
     CHECKPOINTS = [
         # 97,
