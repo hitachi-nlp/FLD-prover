5fafc9e94d527f83ffaef4bf2278e0f5124d3dd3
MorishT <magi.magilmagi@gmail.com>
2024-06-29 02:01:02 +0900

    [exp] ./outputs/01.train.py/2024-06-22.neurip.additional

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -36,10 +36,6 @@ logger = logging.getLogger(__name__)
 @click.command()
 def main():
     setup_logger(level=logging.INFO, clear_other_handlers=True)
-    import time
-    time.sleep(3600 * 6)
-
-
     # output_top_dir = Path('./outputs/01.train.py/2024-4-17.rec_adam')
 
     # output_top_dir = Path('./outputs/01.train.py/2024-4-18.rec_adam')
@@ -69,7 +65,7 @@ def main():
 
 
     # =================================== neurips.additional ===================================
-    # output_top_dir = Path('./outputs/01.train.py/2024-06-22.neurip.additional')
+    output_top_dir = Path('./outputs/01.train.py/2024-06-22.neurip.additional')
 
 
     # =================================== LPT ===================================
@@ -78,7 +74,7 @@ def main():
     # output_top_dir = Path('./outputs/01.train.py/2024-06-19.LPT.zero0'),
     # output_top_dir = Path('./outputs/01.train.py/2024-06-19.LPT.zero0.ALPT')
 
-    output_top_dir = Path('./outputs/01.train.py/2024-0628.ALPT_first')
+    # output_top_dir = Path('./outputs/01.train.py/2024-0628.ALPT_first')
 
 
     # =================================== tranfer ===================================
@@ -134,7 +130,7 @@ def main():
         # ('Qwen/Qwen1.5-72B', 'causal', 'meta-llama/Llama-2-70b-hf'),
 
         # ('mistralai/Mistral-7B-v0.1', 'causal', 'meta-llama/Llama-2-7b-hf'),
-        # ('mistralai/Mixtral-8x7B-v0.1', 'causal', 'meta-llama/Llama-2-70b-hf'),
+        ('mistralai/Mixtral-8x7B-v0.1', 'causal', 'meta-llama/Llama-2-70b-hf'),
 
 
 
@@ -144,7 +140,7 @@ def main():
         # ('EleutherAI/pythia-1b', 'causal', 'EleutherAI/pythia-1b'),
         # ('EleutherAI/pythia-6.9b', 'causal', 'meta-llama/Llama-2-7b-hf'),
         # ('mdl_nm=EleutherAI@pythia-6.9b__lgc_dtst_unm=2024-03-29.JSAI_best.no_aug.trnsl-thing.large__dtst_nms=cerebras@SlimPajama-627B__lgc_dtst_prb=0.0__lrnng=LPT.bs-2048__step-12000.wrmp-100__lrnng_rt=0.0003.chk-12000', 'causal', 'meta-llama/Llama-2-7b-hf')
-        ('mdl_nm=EleutherAI@pythia-6.9b__lgc_dtst_unm=2024-03-29.JSAI_best.no_aug.trnsl-thing.large__dtst_nms=None__lgc_dtst_prb=1.0__lrnng=FT.bs-1024__step-2930.wrmp-300__lrnng_rt=3e-05.chk-2928', 'causal', 'meta-llama/Llama-2-7b-hf'),
+        # ('mdl_nm=EleutherAI@pythia-6.9b__lgc_dtst_unm=2024-03-29.JSAI_best.no_aug.trnsl-thing.large__dtst_nms=None__lgc_dtst_prb=1.0__lrnng=FT.bs-1024__step-2930.wrmp-300__lrnng_rt=3e-05.chk-2928', 'causal', 'meta-llama/Llama-2-7b-hf'),
 
 
         # ============================ transfer     ============================
@@ -246,15 +242,15 @@ def main():
 
         # 'hf.hitachi-nlp/proofwriter_processed_OWA__depth-3ext',
 
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.voc-100',
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.dstrct-0',
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.rule-G_MP',
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.ref_prob=0.10.stps-3-0',
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.transl_sttng-1',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing.voc-100',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing.dstrct-0',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing.rule-G_MP',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing.ref_prob=0.10.stps-3-0',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing.transl_sttng-1',
 
 
         # ---------------------------------- LPT ------------------------------------
-        '2024-03-29.JSAI_best.no_aug.trnsl-thing.large',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.large',
 
 
         # ------------------------------------ transfer ------------------------------------
@@ -278,11 +274,11 @@ def main():
 
         # ------------------------ NeurIPS 2024 -----------------------
 
-        # (
-        #     1.0,
-        #     [],
-        #     False,
-        # ),
+        (
+            1.0,
+            [],
+            False,
+        ),
 
 
         # ------------------------ LPT -----------------------
@@ -295,13 +291,13 @@ def main():
         #     False,
         # ),
 
-        (
-            0.03,
-            [
-                (1.0, 'cerebras/SlimPajama-627B', None, None)
-            ],
-            False,
-        ),
+        # (
+        #     0.03,
+        #     [
+        #         (1.0, 'cerebras/SlimPajama-627B', None, None)
+        #     ],
+        #     False,
+        # ),
 
         # (
         #     0.10,
@@ -354,14 +350,15 @@ def main():
 
 
     learnings = [
+        # -------------------- debug --------------------
         # 'debug.ZS',
         # 'debug.micro',
         # 'debug.tiny',
 
-        # ---- NeurIPS ---------
+        # -------- NeurIPS ------------------
         # 'FT.bs-256__step-390.wrmp-200.few_save',
 
-        # ---- JFLD experiments ----
+        # -------- JFLD experiments --------
         # 'LLM_FS.shot-5',
         # 'LLM_FS.shot-100',
         # 'LLM_FS.shot-1000',
@@ -370,11 +367,11 @@ def main():
 
 
         # ------------------------------ neurip.additional --------------------------------
-        # 'FT.bs-256__step-390.wrmp-200.few_save',
+        'FT.bs-256__step-390.wrmp-200.few_save',
 
 
         # ------------------------------ LPT --------------------------------
-        'LPT.bs-2048__step-12000.wrmp-100',
+        # 'LPT.bs-2048__step-12000.wrmp-100',
         # 'LPT.bs-2048__step-13200.wrmp-132',
         # 'FT.bs-512__step-1952.wrmp-200',     # 1M examples ALPT, 4 nodes
         # 'FT.bs-1024__step-2930.wrmp-300',    # 3M examples ALPT, 8 nodes
@@ -395,9 +392,9 @@ def main():
 
     # (optimizer, regularization, anneal_target_task_weight, fisher_coef)
     optimizer_setings = [
-        (None, None, None),
+        # (None, None, None),
         # ('rec_adam', 1.0, 0),
-        # ('rec_adam', 1.0, 300),
+        ('rec_adam', 1.0, 300),
         # ('rec_adam', 1.0, 1000),
         # ('rec_adam', 1.0, 3000),
         # ('rec_adam', 1.0, 5000),
@@ -408,12 +405,12 @@ def main():
     lrates = [
         #  -------------------------------- Neurips.additional --------------------------------
         # 1e-5,
-        # 3e-6,    # the best for ALPT
+        3e-6,    # the best for ALPT
         # 1e-6,
 
 
         #  -------------------------------- LPT --------------------------------
-        3e-4,
+        # 3e-4,
         # 3e-5,    # ALPT_first
 
 
@@ -443,8 +440,8 @@ def main():
 
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=1)
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=2)
-    # engine = QsubEngine('haic', 'xhn_s.large', n_resource=4)
-    engine = QsubEngine('haic', 'xhn_s.large', n_resource=8)
+    engine = QsubEngine('haic', 'xhn_s.large', n_resource=4)
+    # engine = QsubEngine('haic', 'xhn_s.large', n_resource=8)
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=12)
     hours = 24
 
