11739d2a4e90132506bb412dece907273f3d5e9b
MorishT <magi.magilmagi@gmail.com>
2024-07-03 13:58:25 +0900

    [exp] ./outputs/01.train.py/2024-06-28.sft.others with ALPT

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -235,13 +235,13 @@ def main():
 
 
         # ------------------------------------ transfer ------------------------------------
-        '20240419.20230120.jpn.wordnet_repro_w_proposition.reimpl.D3',
+        # '20240419.20230120.jpn.wordnet_repro_w_proposition.reimpl.D3',
         '2024-03-29.JSAI_best.no_aug.trnsl-thing',
     ]
 
     surface_is_formula_args = [
-        False,
-        # True,
+        # False,
+        True,
     ]
 
 
