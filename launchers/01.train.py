b3f6372af444ec506e3af152307c7559364c631e
MorishT <magi.magilmagi@gmail.com>
2024-07-09 16:03:42 +0900

    [exp] ./outputs/01.train.py/2024-07-09.ALPT.re_run.scratch

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -77,12 +77,12 @@ def main():
 
     # output_top_dir = Path('./outputs/01.train.py/2024-07-08.augmentation')
     # output_top_dir = Path('./outputs/01.train.py/2024-07-08.ALPT_first')
-    output_top_dir = Path('./outputs/01.train.py/2024-07-08.steps')
+    # output_top_dir = Path('./outputs/01.train.py/2024-07-08.steps')
 
     # output_top_dir = Path('./outputs/01.train.py/2024-07-08.ALPT.re_run')
     # output_top_dir = Path('./outputs/01.train.py/2024-07-08.ALPT.re_run.LPT')
 
-    # output_top_dir = Path('./outputs/01.train.py/2024-07-09.ALPT.re_run.scratch')
+    output_top_dir = Path('./outputs/01.train.py/2024-07-09.ALPT.re_run.scratch')
 
 
     # =================================== tranfer ===================================
@@ -152,10 +152,10 @@ def main():
 
         # ======================================================== LPT     ========================================================
 
-        ('meta-llama/Meta-Llama-3-8B', 'causal', 'meta-llama/Llama-2-7b-hf'),
+        # ('meta-llama/Meta-Llama-3-8B', 'causal', 'meta-llama/Llama-2-7b-hf'),
 
         # ('EleutherAI/pythia-1b', 'causal', 'EleutherAI/pythia-1b'),
-        # ('EleutherAI/pythia-6.9b', 'causal', 'meta-llama/Llama-2-7b-hf'),
+        ('EleutherAI/pythia-6.9b', 'causal', 'meta-llama/Llama-2-7b-hf'),
 
         # ALPT after LPT
         # ('mdl_nm=EleutherAI@pythia-6.9b__lgc_dtst_unm=2024-03-29.JSAI_best.no_aug.trnsl-thing.large__dtst_nms=cerebras@SlimPajama-627B__lgc_dtst_prb=0.0__lrnng=LPT.bs-2048__step-12000.wrmp-100__lrnng_rt=0.0003.chk-12000', 'causal', 'meta-llama/Llama-2-7b-hf')
@@ -186,8 +186,8 @@ def main():
     ]
 
     from_scratch_args = [
-        False,
-        # True,
+        # False,
+        True,
     ]
 
 
@@ -280,10 +280,10 @@ def main():
 
 
         # ---------------------------------- LPT ------------------------------------
-        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.large',
+        '2024-03-29.JSAI_best.no_aug.trnsl-thing.large',
 
-        '2024-03-29.JSAI_best.no_aug.trnsl-thing.steps',
-        '2024-03-29.JSAI_best.no_aug.trnsl-thing.steps-5',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.steps',
+        # '2024-03-29.JSAI_best.no_aug.trnsl-thing.steps-5',
 
 
         # ------------------------------------ transfer ------------------------------------
@@ -480,7 +480,7 @@ def main():
 
 
         # =========================== LPT ===========================
-        'FT.bs-256__step-390.wrmp-200.few_save',
+        # 'FT.bs-256__step-390.wrmp-200.few_save',
 
         # 'LPT.bs-2048__step-12000.wrmp-100',
         # 'LPT.bs-2048__step-12000.wrmp-1000',
@@ -488,6 +488,7 @@ def main():
 
         # 'FT.bs-512__step-1952.wrmp-200',     # 1M examples ALPT, 4 nodes
         # 'FT.bs-1024__step-2930.wrmp-300',    # 3M examples ALPT, 8 nodes
+        'FT.bs-1024__step-9765.wrmp-300',    # 10M examples ALPT, 8 nodes
 
 
         # =========================== transfer ===========================
@@ -524,9 +525,10 @@ def main():
         #  -------------------------------- LPT --------------------------------
         # 3e-4,    # PT, LPT
         # 3e-5,    # ALPT (scratch)
+        1e-4,    # ALPT (scratch)
         # 1e-4,    # PT after ALPT
 
-        3e-6,
+        # 3e-6,
         # 1e-6,
 
         #  -------------------------------- RWKV --------------------------------
@@ -549,11 +551,11 @@ def main():
 
     # (optimizer, regularization, anneal_target_task_weight, fisher_coef)
     optimizer_setings = [
-        # (None, None, None),
+        (None, None, None),
         # ('rec_adam', 1.0, 0),
         # ('rec_adam', 1.0, 300),
         # ('rec_adam', 1.0, 1000),
-        ('rec_adam', 1.0, 3000),
+        # ('rec_adam', 1.0, 3000),
         # ('rec_adam', 1.0, 5000),
         # ('rec_adam', 1.0, 10000),
     ]
@@ -571,18 +573,18 @@ def main():
     # engine = SubprocessEngine('haic', 'xhn_s.large', n_resource=1)
 
 
-    engine = QsubEngine('haic', 'xhn_s.large', n_resource=1)
+    # engine = QsubEngine('haic', 'xhn_s.large', n_resource=1)
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=2)
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=4)
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=8)
     # engine = QsubEngine('haic', 'xhn_s.large', n_resource=12)
-    hours = 12
+    # hours = 12
 
     # engine = QsubEngine('haic', 'xhn_l.large', n_resource=1)
     # engine = QsubEngine('haic', 'xhn_l.large', n_resource=2)
     # engine = QsubEngine('haic', 'xhn_l.large', n_resource=4)
-    # engine = QsubEngine('haic', 'xhn_l.large', n_resource=8)
-    # hours = 72
+    engine = QsubEngine('haic', 'xhn_l.large', n_resource=8)
+    hours = 72
 
     # skip_if_exists = False
     skip_if_exists = True
