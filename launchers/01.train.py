94f2dcbb69cbefde7ffee599ba9129aae2433576
MorishT <magi.magilmagi@gmail.com>
2024-09-08 23:58:21 +0900

    [update]

--- launchers/01.train.py
+++ launchers/01.train.py
@@ -136,7 +136,7 @@ def main():
 
     # =================================== 2024-09-06.llama3 ========================================
     # output_top_dir = Path('./outputs/01.train.py/2024-09-03.toward_camera_ready')
-    # output_top_dir = Path('./outputs/01.train.py/2024-09-06.llama3')
+    output_top_dir = Path('./outputs/01.train.py/2024-09-06.llama3')
 
 
 
@@ -151,7 +151,7 @@ def main():
 
 
     # =================================== 2024-09-06.factorized_reasoning ========================================
-    output_top_dir = Path('./outputs/01.train.py/2024-09-06.factorized_reasoning')
+    # output_top_dir = Path('./outputs/01.train.py/2024-09-06.factorized_reasoning')
 
 
 
@@ -161,7 +161,7 @@ def main():
         # ('TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T', 'causal', 'cyberagent/open-calm-3b'),
 
         # ('meta-llama/Meta-Llama-3-8B', 'causal', 'meta-llama/Llama-2-7b-hf'),
-        # ('meta-llama/Meta-Llama-3-70B', 'causal', 'meta-llama/Llama-2-7b-hf'),
+        ('meta-llama/Meta-Llama-3-70B', 'causal', 'meta-llama/Llama-2-7b-hf'),
 
         # ('meta-llama/Meta-Llama-3.1-8B', 'causal', 'meta-llama/Llama-2-7b-hf'),
         # ('meta-llama/Meta-Llama-3.1-70B', 'causal', 'meta-llama/Llama-2-70b-hf'),
@@ -182,7 +182,7 @@ def main():
 
 
         # ======================================================== 2024-09-06.factorized_reasoning     ========================================================
-        ('mdl_nm=meta-llama@Meta-Llama-3.1-8B__lgc_dtst_unm=2024-09-03.trnsl-thing_person-v0__optmzr=rec_adam__lrnng=FT.bs-256__step-1953.wrmp-200__lrnng_rt=1e-05__rc_adm_fshr_cf=1000__augmnttn=False__prf_intrmdt_stps=randomly_include__prf_intrmdt_stps_prb=0.5__mx_grd_nrm=0.5__prmpt_indct_thrms=True.chk-1953', 'causal', 'meta-llama/Llama-2-7b-hf'),
+        # ('mdl_nm=meta-llama@Meta-Llama-3.1-8B__lgc_dtst_unm=2024-09-03.trnsl-thing_person-v0__optmzr=rec_adam__lrnng=FT.bs-256__step-1953.wrmp-200__lrnng_rt=1e-05__rc_adm_fshr_cf=1000__augmnttn=False__prf_intrmdt_stps=randomly_include__prf_intrmdt_stps_prb=0.5__mx_grd_nrm=0.5__prmpt_indct_thrms=True.chk-1953', 'causal', 'meta-llama/Llama-2-7b-hf'),
 
     ]
 
@@ -202,6 +202,7 @@ def main():
         # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.20.theorem-G_MP.syllogism.contraposition',
         # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.20.theorem-G_MP.syllogism.contraposition.interchangeability',
         # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.13.theorem-G_MP.syllogism.contraposition.interchangeability',
+        # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.05.theorem-G_MP.syllogism.contraposition.interchangeability',
         # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.20.theorem-all',
 
         # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.066.theorem-G_MP',
@@ -210,13 +211,16 @@ def main():
 
         # ====================================== 2024-09-03.toward_camera_ready ============================
 
-        # 'hf.hitachi-nlp/ruletaker',
+        'hf.hitachi-nlp/ruletaker',
         # 'hf.hitachi-nlp/PARARULE-Plus',
-
         # '2024-08-30.FLD.ref_prob-0.20',
-        # '2024-09-03.trnsl-thing_person-v2',
 
         # '2024-09-03.trnsl-thing_person-v0',
+        # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.20.theorem-G_MP.syllogism.contraposition.interchangeability',
+        # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.13.theorem-G_MP.syllogism.contraposition.interchangeability',
+        # '2024-08-30.trnsl-thing_person-v0.ref_prob-0.05.theorem-G_MP.syllogism.contraposition.interchangeability',
+
+        # '2024-09-03.trnsl-thing_person-v2',
 
 
 
@@ -234,7 +238,7 @@ def main():
         # 'AUG__2024-08-09.depth_fix.2024-03-29.FLD_v2.trnsl-thing_person-v0__augmnttn=True__augmnttn_prb=0.5__llm_nm=hf.TechxGenus@Mistral-Large-Instruct-2407-AWQ__prf_intrmdt_stps=randomly_include',
 
         # =================== 2024-09-06.factorized_reasoning ======================
-        'hf.hitachi-nlp/ruletaker',
+        # 'hf.hitachi-nlp/ruletaker',
 
     ]
 
@@ -244,18 +248,18 @@ def main():
         # [datasetライブラリで大規模データセットを扱う]($PROJECTS/NLP/LLM.md)
         # ================================================ NeurIPS 2024 ==============================================
 
-        # (
-        #     1.0,
-        #     [],
-        # ),
-
-
         (
-            0.0,
-            [
-                (1.0, 'FR.2024-09-06.large_train', None, None, None)
-            ],
+            1.0,
+            [],
         ),
+
+
+        # (
+        #     0.0,
+        #     [
+        #         (1.0, 'FR.2024-09-06.large_train', None, None, None)
+        #     ],
+        # ),
 
 
         # ================================================ ALPT_strong ==============================================
@@ -278,13 +282,14 @@ def main():
         # ),
     ]
 
-    do_sft = True
-    # do_sft = False
+    # do_sft = True
+    do_sft = False
+
 
 
 
     learnings = [
-        # 'debug.FT.bs-64__step-10.wrmp-0',
+        'debug.FT.bs-64__step-10.wrmp-0',
         # 'FT.bs-256__step-98.wrmp-50',  # 25k examples
         # 'FT.bs-256__step-195.wrmp-100',  # 50k examples
         # 'FT.bs-256__step-390.wrmp-200',  # 100k examples
@@ -297,7 +302,7 @@ def main():
 
         # ========== factoorized reasoning ==========
         # https://github.com/tatsu-lab/stanford_alpaca
-        'FT.bs-128__step-94.wrmp-20',  # 4k examples x 3 epochs
+        # 'FT.bs-128__step-94.wrmp-20',  # 4k examples x 3 epochs
     ]
 
     proof_intermediate_steps_prob_args = [
@@ -322,7 +327,7 @@ def main():
 
     lrates = [
         3e-6,
-        # 1e-5,     # the best on 100k examples
+        # 1e-5,
     ]
 
 
@@ -334,13 +339,13 @@ def main():
         # ('rec_adam', 1.0, 'auto'),
 
         # ('rec_adam', 1.0, 0),
-        # ('rec_adam', 1.0, 300),
+        ('rec_adam', 1.0, 300),
         # ('rec_adam', 1.0, 1000),
         # ('rec_adam', 1.0, 3000),
         # ('rec_adam', 1.0, 5000),
         # ('rec_adam', 1.0, 10000),
 
-        (None, None, None),
+        # (None, None, None),
         # ('adamw_hf', None, None),
     ]
 
@@ -353,9 +358,9 @@ def main():
     # hours = 24
 
 
-    engine = QsubEngine('haic', 'xhn_s.middle2', n_resource=1)
+    # engine = QsubEngine('haic', 'xhn_s.middle2', n_resource=1)
     # engine = QsubEngine('haic', 'xhn_s.middle2', n_resource=2)
-    # engine = QsubEngine('haic', 'xhn_s.middle2', n_resource=4)
+    engine = QsubEngine('haic', 'xhn_s.middle2', n_resource=4)
     hours = 12
 
 
